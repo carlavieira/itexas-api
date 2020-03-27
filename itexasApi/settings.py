067e3c70c7fa6561e3c3dde825615d06e318330f
Daniel Lyncon <43709270+daniellyncon@users.noreply.github.com>
2020-03-27 18:10:04 -0300

    adding CORS configuration

--- itexasApi/settings.py
+++ itexasApi/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'corsheaders',
     'membersApi',
     'postApi',
     'departmentApi',
@@ -77,6 +78,7 @@ REST_FRAMEWORK = {
 REST_USE_JWT = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -84,7 +86,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'itexasApi.urls'
 
