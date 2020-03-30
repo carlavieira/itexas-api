fc0d3e8097986c11381d60febda3f4a9e025f67f
Carla Vieira <carla.vieira@mlb.com>
2020-03-30 20:53:00 -0300

    adding nickname to members

--- membersApi/serializers.py
+++ membersApi/serializers.py
@@ -5,5 +5,5 @@ from .models import Member
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = ('email', 'first_name', 'last_name', 'post', 'department', 'leader', 'photo', 'slack', 'phone')
+        fields= ('email', 'first_name', 'last_name', 'nickname', 'post', 'department', 'leader', 'photo', 'slack', 'phone')
         extra_kwargs = {'password': {'write_only': True}}
