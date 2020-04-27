b19b5785cc9a76b9db02d860a82ff509e183c803
Carla Vieira <carla.vieira@mlb.com>
2020-04-27 18:26:30 -0300

    Updates status

--- membershipCriteria/models.py
+++ membershipCriteria/models.py
@@ -1,5 +1,7 @@
 from datetime import date
 from django.db import models
+from django.db.models.signals import post_save
+
 from membersApi.models import Member
 
 
@@ -10,4 +12,19 @@ class MembershipCriteria(models.Model):
     officeHoursCriteria = models.DecimalField(verbose_name='Critério de Office Hour', max_digits=5, decimal_places=2)
     meetingsCriteria = models.DecimalField(verbose_name='Critério de Reuniões', max_digits=5, decimal_places=2)
     eventsCriteria = models.DecimalField(verbose_name='Critério de Eventos', max_digits=5, decimal_places=2)
+    status = models.TextField(verbose_name='Status', default='-')
+
+def updateStatus(instance, **kwargs):
+    if instance.officeHoursCriteria<100 or instance.meetingsCriteria<75 or instance.eventsCriteria<50:
+        instance.status = "RISCO"
+        post_save.disconnect(updateStatus, sender=MembershipCriteria)
+        instance.save()
+
+    if instance.officeHoursCriteria>=100 and instance.meetingsCriteria>=75 and instance.eventsCriteria>=50:
+        instance.status = "IDEAL"
+        post_save.disconnect(updateStatus, sender=MembershipCriteria)
+        instance.save()
+
+    post_save.connect(updateStatus, sender=MembershipCriteria)
+
 
