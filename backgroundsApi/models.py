c777478b3d746106a5e43a8e136092e668c3649a
Carla Vieira <carla.vieira@mlb.com>
2020-05-18 23:47:21 -0300

    Adds background description

--- backgroundsApi/models.py
+++ backgroundsApi/models.py
@@ -11,3 +11,4 @@ class Background(models.Model):
     end_date = models.DateField(verbose_name='Término', default=date.today)
     post = models.ForeignKey(Post, blank=True, null=True, on_delete=models.SET_NULL, verbose_name='Cargo')
     department = models.ForeignKey(Department, blank=True, null=True, on_delete=models.SET_NULL, verbose_name='Área')
+    description = models.CharField(verbose_name='Descrição', max_length=250, blank=True)
