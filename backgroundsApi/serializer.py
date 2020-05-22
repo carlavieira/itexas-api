bd0b6137508438c23e56db15a9305ae6896d14a4
Carla Vieira <carla.vieira@mlb.com>
2020-05-22 00:27:33 -0300

    Updates backgroud

--- backgroundsApi/serializer.py
+++ backgroundsApi/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Background
 from departmentsApi.models import Department
 from postsApi.models import Post
+from membersApi.models import Member
 
 
 class BackgroundSerializer(serializers.ModelSerializer):
@@ -11,6 +12,8 @@ class BackgroundSerializer(serializers.ModelSerializer):
         depth = 9
 
 class ManageBackgroundSerializer(serializers.ModelSerializer):
+    member = serializers.PrimaryKeyRelatedField(required=True, many=False,
+                                                    read_only=False, queryset=Member.objects.all())
     post = serializers.PrimaryKeyRelatedField(required=False, many=False,
                                               read_only=False, queryset=Post.objects.all())
     department = serializers.PrimaryKeyRelatedField(required=False, many=False,
