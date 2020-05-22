7a8430eed9e1566f7c277a48b0c062ed59aba9d2
Carla Vieira <carla.vieira@mlb.com>
2020-05-22 00:30:49 -0300

    Updates backgroud

--- backgroundsApi/views.py
+++ backgroundsApi/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from .models import Background
 from .serializer import BackgroundSerializer, ManageBackgroundSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ListBackgroundViewSet(viewsets.ModelViewSet):
     queryset = Background.objects.all()
     serializer_class = BackgroundSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ('member',)
 
 class ManageBackgroundViewSet(viewsets.ModelViewSet):
     queryset = Background.objects.all()
